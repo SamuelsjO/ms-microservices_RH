e47b576c13aac54654fcefd6c9ec2d5f070a6260
Samuel Jose de Souza Oliveira <soliveira@cpqd.com.br>
2021-10-28 19:22:29 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/stecnology/hrpayroll/resources/PaymentResources.java
+++ hr-payroll/src/main/java/com/stecnology/hrpayroll/resources/PaymentResources.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.stecnology.hrpayroll.entities.Payment;
 import com.stecnology.hrpayroll.services.PaymentService;
 
@@ -17,9 +18,16 @@ public class PaymentResources {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workedId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workedId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workedId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workedId, Integer days){
+		Payment payment = new Payment("Samuel",400.0, days);
 		return ResponseEntity.ok(payment);
 	}
 
