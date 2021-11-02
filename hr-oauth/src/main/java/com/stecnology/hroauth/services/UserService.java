bc2f5286d2403f8370adac5bdf4d0c246ff81db4
Samuel Jose de Souza Oliveira <soliveira@cpqd.com.br>
2021-11-02 19:28:57 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/stecnology/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/stecnology/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.stecnology.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.stecnology.hroauth.entities.UserDTO;
 import com.stecnology.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException(username);
+		}
+		logger.info("User found: " + username);
 		return user;
 	}
 }
